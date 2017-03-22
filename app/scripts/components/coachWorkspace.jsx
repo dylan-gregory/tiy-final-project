21d5f356b3f910fd3a6c904e9e7c581ee9ea0bab
dylan-gregory <jdylangregory@gmail.com>
2017-03-22 16:50:19 -0400

    Still wrestling

--- app/scripts/components/coachWorkspace.jsx
+++ app/scripts/components/coachWorkspace.jsx
@@ -150,10 +150,7 @@ class CoachWorkspaceContainer extends React.Component{
               <div className="col s4">
                 <h2>Leaderboard</h2>
 
-                <ClientStarChart
-                  clientStars={this.state.clientStars}
-                  clientNames={this.state.clientNames}
-                />
+
 
                   <ClientLeaderBoard
                     clientCollection={this.state.clientCollection}
@@ -169,6 +166,11 @@ class CoachWorkspaceContainer extends React.Component{
     )
   }
 }
+
+// <ClientStarChart
+//   clientStars={this.state.clientStars}
+//   clientNames={this.state.clientNames}
+// />
 
 class CoachClientList extends React.Component {
   constructor(props){
@@ -333,69 +335,50 @@ class ClientLeaderBoard extends React.Component {
   }
 }
 
-class ClientStarChart extends React.Component {
-  constructor(props){
-    super(props);
-    var clientNames = [];
-    var clientStars = [];
-    var chartData = {};
-
-    this.setState = {
-      chartData,
-      clientNames,
-      clientStars
-    };
-    //
-    // this.componentWillReceiveProps = this.componentWillReceiveProps.bind(this);
-
-  }
-  // componentWillReceiveProps(newProps){
-  //
-  // // var newChartData = {
-  // //       labels: newProps.clientNames,
-  // //       datasets: [{
-  // //           data: newProps.clientStars,
-  // //           backgroundColor: [
-  // //               'rgba(255,99,132,1)',
-  // //               'rgba(54, 162, 235, 1)',
-  // //               'rgba(255, 206, 86, 1)',
-  // //               'rgba(75, 192, 192, 1)',
-  // //               'rgba(153, 102, 255, 1)',
-  // //               'rgba(255, 159, 64, 1)',
-  // //               'rgba(255, 159, 64, 1)',
-  // //               'rgba(255, 159, 64, 1)',
-  // //               'rgba(255, 159, 64, 1)'
-  // //           ],
-  // //           borderWidth: 1
-  // //       }]
-  // //   };
-  //
-  //   this.setState({clientNames: newProps.clientNames, clientStars: newProps.clientStars});
-  //
-  // }
-  render(){
-
-    var chartData = {
-        labels: this.props.clientNames,
-        datasets: [{
-            data: this.props.clientStars,
-            backgroundColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-                'rgba(255, 159, 64, 1)',
-                'rgba(255, 159, 64, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    }
-    return <PolarAreaChart data={chartData} />
-  }
-}
+// class ClientStarChart extends React.Component {
+//   constructor(props){
+//     super(props);
+//     var clientNames = ['tom', 'joe'];
+//     var clientStars = [1, 2];
+//     var chartData = {};
+//
+//     this.setState = {
+//       chartData,
+//       clientNames,
+//       clientStars
+//     };
+//
+//     this.componentWillReceiveProps = this.componentWillReceiveProps.bind(this);
+//
+//   }
+//   componentWillReceiveProps(newProps){
+//
+//     this.setState({clientNames: newProps.clientNames, clientStars: newProps.clientStars});
+//
+//   }
+//   render(){
+//
+//     var chartData = {
+//         labels: this.state.clientNames,
+//         datasets: [{
+//             data: this.state.clientStars,
+//             backgroundColor: [
+//                 'rgba(255,99,132,1)',
+//                 'rgba(54, 162, 235, 1)',
+//                 'rgba(255, 206, 86, 1)',
+//                 'rgba(75, 192, 192, 1)',
+//                 'rgba(153, 102, 255, 1)',
+//                 'rgba(255, 159, 64, 1)',
+//                 'rgba(255, 159, 64, 1)',
+//                 'rgba(255, 159, 64, 1)',
+//                 'rgba(255, 159, 64, 1)'
+//             ],
+//             borderWidth: 1
+//         }]
+//     }
+//     return <PolarAreaChart data={chartData} />
+//   }
+// }
 
 
 
